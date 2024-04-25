5ce1727592cb6f59e9bb54e635772e60284d8fe1
Lam <lamkyo373@gmail.com>
2024-04-25 18:42:05 +0700

    commit buoi_1

---
+++ src/buoi1/index.js
@@ -0,0 +1,26 @@
+console.log("lien ket thanh cong");
+var a = 3;
+var b = 4;
+var canhHuyen = Math.sqrt(a * a + b * b);
+console.log(canhHuyen);
+
+//bai 2
+// var n = 586;
+
+// var chuSoThuNhat = 586 / 100;
+// console.log(chuSoThuNhat);
+// tinh tien luong
+// var luongMotNgay = 100000;
+// var soNgayLam = +prompt("nhap so ngay");
+// var tienLuong = 100000 * soNgayLam;
+// console.log(tienLuong);
+// gia tri trung binh
+// var soThuNhat = +prompt("nhap so thu nhat: ");
+// var soThuHai = +prompt("nhap so thu hai: ");
+// var soThuBa = +prompt("nhap so thu ba: ");
+// var soThuTu = +prompt("nhap so thu tu: ");
+// var soThuNam = +prompt("nhap so thu nam: ");
+
+// var trungBinhCong = soThuNhat + soThuHai + soThuBa + soThuTu + soThuNam / 5;
+// console.log(trungBinhCong);
+console.log(4 + "3" + 5 + 2 + 1);
